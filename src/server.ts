e2a6aa4646ed27696e31eb4ca5a8a3d5740d20fe
StanislavKharkiv <s.iosifov@quantumobile.com>
2023-01-16 18:48:26 +0200

    feat: add worker

--- src/server.ts
+++ src/server.ts
@@ -9,60 +9,63 @@ const usersData = new UserController();
 export function server(port: number) {
   async function requestListener(req: IncomingMessage, res: ServerResponse) {
     const response = new userResponse(res);
-
-    // USERS
-    if (req.url === "/api/users") {
-      // USERS GET
-      if (req.method === "GET") {
-        response.send(200, JSON.stringify(usersData.getUsers()));
-      }
-      // USER POST
-      else if (req.method === "POST") {
-        const reqBody = await getReqData(req);
-        if (usersData.addUser(reqBody)) {
-          response.send(201, "User added!");
-        } else {
-          response.send(400, "Body does not contain required fields!");
+    try {
+      // USERS
+      if (req.url === "/api/users") {
+        // USERS GET
+        if (req.method === "GET") {
+          response.send(200, JSON.stringify(usersData.getUsers()));
         }
-      } else {
+        // USER POST
+        else if (req.method === "POST") {
+          const reqBody = await getReqData(req);
+          if (usersData.addUser(reqBody)) {
+            response.send(201, "User added!");
+          } else {
+            response.send(400, "Body does not contain required fields!");
+          }
+        } else {
+          response.notFound();
+        }
+      }
+      // USER BY ID
+      else if (req.url?.match(/\/api\/users\/([0-9a-fA-F])/)) {
+        const userId = req.url.split("/")[3];
+        if (!validate(userId)) {
+          response.send(400, "Invalid user id");
+          return;
+        }
+        switch (req.method) {
+          case "GET": {
+            const user = usersData.getUser(userId);
+            if (user) {
+              response.send(200, JSON.stringify(user));
+            } else response.noUserResp();
+            break;
+          }
+          case "PUT": {
+            const reqBody = await getReqData(req);
+            if (usersData.putUser(reqBody, userId)) {
+              response.send(200, "User updated!");
+            } else response.noUserResp();
+            break;
+          }
+          case "DELETE": {
+            if (usersData.deleteUser(userId)) {
+              response.send(204);
+            } else response.noUserResp();
+            break;
+          }
+          default:
+            response.notFound();
+        }
+      }
+      // 404 not found
+      else {
         response.notFound();
       }
-    }
-    // USER BY ID
-    else if (req.url?.match(/\/api\/users\/([0-9a-fA-F])/)) {
-      const userId = req.url.split("/")[3];
-      if (!validate(userId)) {
-        response.send(400, "Invalid user id");
-        return;
-      }
-      switch (req.method) {
-        case "GET": {
-          const user = usersData.getUser(userId);
-          if (user) {
-            response.send(200, JSON.stringify(user));
-          } else response.noUserResp();
-          break;
-        }
-        case "PUT": {
-          const reqBody = await getReqData(req);
-          if (usersData.putUser(reqBody, userId)) {
-            response.send(200, "User updated!");
-          } else response.noUserResp();
-          break;
-        }
-        case "DELETE": {
-          if (usersData.deleteUser(userId)) {
-            response.send(204);
-          } else response.noUserResp();
-          break;
-        }
-        default:
-          response.notFound();
-      }
-    }
-    // 404 not found
-    else {
-      response.notFound();
+    } catch {
+      response.send(500, "Something went wrong.");
     }
   }
 
